55601b2d441fd055f97c32d8e276bf08d68a8bdc
wonsski <radek.rajda@hotmail.com>
2023-01-21 21:47:43 +0100

    gen

--- generator.py
+++ generator.py
@@ -1,13 +1,13 @@
 import random
 
 zakres = 10
-n = 1000
+n = 300
 
 poprzednia = 1
 
 f = open("duzy_plik.txt", "w")
 for i in range(1,n):
-    nastepna = random.randint(1,zakres)
-    f.write(f"({poprzednia}->{nastepna}: {random.randint(1,zakres)*1.0}), \n")
+    nastepna = random.randint(2,zakres)
+    f.write(f"({poprzednia}-{nastepna}: {random.randint(1,zakres)*1.0}), \n")
     poprzednia = nastepna
 f.write(f"({poprzednia}->1: 1.0) \n")
