6160de4d78b152881a0f464169070bf1e3491cf0
wonsski <radek.rajda@hotmail.com>
2023-01-22 19:10:02 +0100

    ulepszony generator

--- generator.py
+++ generator.py
@@ -7,7 +7,12 @@ poprzednia = 1
 
 f = open("duzy_plik.txt", "w")
 for i in range(1,n):
-    nastepna = random.randint(2,zakres)
-    f.write(f"({poprzednia}-{nastepna}: {random.randint(1,zakres)*1.0}), \n")
-    poprzednia = nastepna
-f.write(f"({poprzednia}->1: 1.0) \n")
+    klientB = random.randint(1,zakres)
+    klientA = random.randint(1,zakres)
+
+    czyJednokierunkowa = random.randint(1,zakres)%2==0
+
+    if czyJednokierunkowa:
+        f.write(f"({klientA}->{klientB}: {random.randint(1,zakres)*1.0}), \n")
+    else:
+        f.write(f"({klientA}-{klientB}: {random.randint(1,zakres)*1.0}), \n")
